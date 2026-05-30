9c31832aa3c1ae33e3d76de30a171e4fdce05c74
AlesioCragno <alefacragno@yahoo.com>
2026-05-30 19:31:49 -0300

    se completo PUT

--- routes/alumno.routes.js
+++ routes/alumno.routes.js
@@ -1,12 +1,15 @@
 const { Router } = require('express')
+const { alumnoValidator } = require('../middleware/alumno-validator.middleware')
 const {
   getAlumnoAll,
-  getAlumnoById
+  getAlumnoById,
+  putAlumnoById
 } = require('../controllers/alumno.controller')
 
 const rutas = Router()
 
 rutas.get('/', getAlumnoAll)
 rutas.get('/:legajo', getAlumnoById)
+rutas.put('/:legajo', alumnoValidator, putAlumnoById)
 
 module.exports = rutas
